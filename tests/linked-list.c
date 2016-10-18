4b575a4fae9f575ec978e0ac66dc84fea3e2e965
Matthew Tole <mtole@pebble.com>
2016-10-18 10:50:19 -0700

    Add Emery support and update to 1.4.0 (demo to 1.1.0)

--- tests/linked-list.c
+++ tests/linked-list.c
@@ -2,7 +2,7 @@
 #include "unit.h"
 #include "linked-list.h"
 
-#define VERSION_LABEL "1.3.0"
+#define VERSION_LABEL "1.4.0"
 
 // Colour code definitions to make the output all pretty.
 #define KNRM  "\x1B[0m"
